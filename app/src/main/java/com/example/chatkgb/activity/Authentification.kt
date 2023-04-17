934a35816f3769b37613d8d02c5516dc3ca1bc17
KNJSoft <knjprod@gmail.com>
2023-04-17 04:00:06 +0100

    inscription/authentification ajouté

--- app/src/main/java/com/example/chatkgb/activity/Authentification.kt
+++ app/src/main/java/com/example/chatkgb/activity/Authentification.kt
@@ -9,8 +9,12 @@ import com.example.chatkgb.Chat
 import com.example.chatkgb.R
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Authentification : AppCompatActivity() {
+    private lateinit var fbauth: FirebaseAuth
     lateinit var inscription: TextView
     lateinit var emailauth: TextInputEditText
     lateinit var passwordauth:TextInputEditText
@@ -18,10 +22,16 @@ class Authentification : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentification)
+        fbauth = Firebase.auth
         inscription=findViewById(R.id.inscription)
         emailauth=findViewById(R.id.emailauth)
         passwordauth=findViewById(R.id.passwordauth)
         auth=findViewById(R.id.auth)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
         inscription.setOnClickListener{
             Intent(this,Inscription::class.java).also{
                 startActivity(it)
@@ -34,28 +44,37 @@ class Authentification : AppCompatActivity() {
             passwordauth.isEnabled=false
             emailauth.isEnabled=false
 
-        if (email.isEmpty() || password.isEmpty()){
-            if(password.isEmpty()){
-                passwordauth.error="Le mot de passe est obligatoire!!!"
-                passwordauth.isEnabled=true
+            if (email.isEmpty() || password.isEmpty()){
+                if(password.isEmpty()){
+                    passwordauth.error="Le mot de passe est obligatoire!!!"
+                    passwordauth.isEnabled=true
+                }
+                if(email.isEmpty()){
+                    emailauth.error="L'email de passe est obligatoire!!!"
+                    emailauth.isEnabled=true
+                }
+            }else{
+                sigin(email,password)
+                fbauth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Intent(this, MainActivity::class.java).also {
+                            startActivity(it)
+                        }
+                        finish()
+                    }else{
+                        passwordauth.error="Authentification echoué!!!"
+                        emailauth.isEnabled=true
+                        passwordauth.isEnabled=true
+                    }
+                }
             }
-            if(email.isEmpty()){
-                emailauth.error="L'email de passe est obligatoire!!!"
-                emailauth.isEnabled=true
-            }
-        }else{
-            sigin(email,password)
-        }
 
-    }
+        }
     }
     fun sigin(email:String,password:String){
-        Log.d("signin","signin")
+        /*Log.d("signin","signin")
         if (email=="knjprod@gmail.com" && password=="azerty") {
-            Intent(this, MainActivity::class.java).also {
-                startActivity(it)
-            }
-            finish()
-        }
+
+        }*/
     }
 }
--- app/src/main/java/com/example/chatkgb/activity/Inscription.kt
+++ app/src/main/java/com/example/chatkgb/activity/Inscription.kt
@@ -1,12 +1,79 @@
 package com.example.chatkgb.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.chatkgb.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Inscription : AppCompatActivity() {
+    private lateinit var fbauth: FirebaseAuth
+    lateinit var Nom: TextInputEditText
+    lateinit var mail: TextInputEditText
+    lateinit var password: TextInputEditText
+    lateinit var confirmpassword: TextInputEditText
+    lateinit var ins: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inscription)
+        fbauth = Firebase.auth
+        Nom=findViewById(R.id.Nom)
+        mail=findViewById(R.id.Mail)
+        password=findViewById(R.id.Password)
+        confirmpassword=findViewById(R.id.confirpassword)
+        ins=findViewById(R.id.ins)
+        ins.setOnClickListener{
+            initerrors()
+            val nom=Nom.editableText.toString()
+            val email=mail.editableText.toString()
+            val pwd=password.editableText.toString()
+            val confirmpwd=confirmpassword.editableText.toString()
+            if(nom.isEmpty()||email.isEmpty()||pwd.isEmpty()||confirmpwd.isEmpty()){
+                if (nom.isEmpty()){
+                    Nom.error="Le nom est obligatoire!!!"
+                    Nom.isEnabled=true
+                }
+                if (email.isEmpty()){
+                    mail.error="L'email est obligatoire!!!"
+                    mail.isEnabled=true
+                }
+                if (pwd.isEmpty()){
+                    password.error="Le mot de passe est obligatoire!!!"
+                    password.isEnabled=true
+                }
+                if (confirmpwd.isEmpty()){
+                    confirmpassword.error="Le mot de passe de confirmation est obligatoire!!!"
+                    confirmpassword.isEnabled=true
+                }
+            }else{
+                if (pwd!=confirmpwd){
+                    confirmpassword.error="Mot de passe incorrect!!!"
+                    confirmpassword.isEnabled=true
+                }else{
+                    fbauth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            Intent(this, MainActivity::class.java).also {
+                                startActivity(it)
+                            }
+                            finish()
+                        }else{
+                            confirmpassword.error="Erreur survenue!!!"
+                            confirmpassword.isEnabled=true
+                        }
+                    }
+                }
+            }
+        }
+    }
+    fun initerrors(){
+        Nom.isEnabled=false
+        mail.isEnabled=false
+        password.isEnabled=false
+        confirmpassword.isEnabled=false
     }
 }
