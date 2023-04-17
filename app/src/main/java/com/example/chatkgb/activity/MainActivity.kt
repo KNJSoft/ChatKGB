c2b0609c7c8f482be742aa8ce17f3af588ba72b1
KNJSoft <knjprod@gmail.com>
2023-04-17 02:54:59 +0100

    integration de firebase

--- app/src/main/java/com/example/chatkgb/activity/MainActivity.kt
+++ app/src/main/java/com/example/chatkgb/activity/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.chatkgb.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.chatkgb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.logout){
+            Intent(this,Authentification::class.java).also {
+                startActivity(it)
+            }
+            finish()
+        }
+        if (item.itemId==R.id.settings){
+            /*Intent(this,settings::class.java).also {
+                startActivity(it)
+            }*/
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
