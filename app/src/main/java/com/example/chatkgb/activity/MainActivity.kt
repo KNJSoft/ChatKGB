2a47204293878475f9d567c82d00766cf80fa09b
KNJSoft <knjprod@gmail.com>
2023-05-07 05:21:15 +0100

    add item_friend recycleview

--- app/src/main/java/com/example/chatkgb/activity/MainActivity.kt
+++ app/src/main/java/com/example/chatkgb/activity/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.chatkgb.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chatkgb.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var liste_amis: RecyclerView
+    lateinit var chat: FloatingActionButton
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        liste_amis=findViewById(R.id.liste_amis)
+        chat=findViewById(R.id.chat)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
